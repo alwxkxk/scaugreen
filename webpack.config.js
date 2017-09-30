cbf9a99109998a7c733dbd2a3103446611a9cd67
生子当如哈士奇 <592364224@qq.com>
2017-09-30 16:50:35 +0800

    添加了vue-route 与下拉菜单

--- webpack.config.js
+++ webpack.config.js
@@ -13,7 +13,26 @@ module.exports = {
   },
   module: {
     rules: [
-      { test: /\.js$/, exclude: /node_modules/, loader: 'babel-loader' }
+      {
+        test: /\.js$/,
+        loader: 'babel-loader',
+        exclude: /node_modules/
+      },
+      {
+        test: /\.css$/,
+        loader: 'style-loader!css-loader'
+      },
+      {
+        test: /\.(eot|svg|ttf|woff|woff2)(\?\S*)?$/,
+        loader: 'file-loader'
+      },
+      {
+        test: /\.(png|jpe?g|gif|svg)(\?\S*)?$/,
+        loader: 'file-loader',
+        query: {
+          name: '[name].[ext]?[hash]'
+        }
+      }
     ],
     loaders: [
       {
